f89863054b20031a84d990aa2cdea202fe6744a5
BradHubley <brad.hubley@dfo-mpo.gc.ca>
2017-11-14 15:36:47 -0400

    adding data

---
+++ R/mapdata2.R
@@ -0,0 +1,18 @@
+#' Mapping data: planar
+#'
+#'
+#' @docType data
+#'
+#' @usage data(mapdata2)
+#'
+#' @format objects 
+#'
+#' @keywords datasets
+#'
+#' @references 
+#'
+#' @source 
+#'
+#' @examples
+#' data(mapdata2)
+"mapdata2"
