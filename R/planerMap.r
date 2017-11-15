edfa771734867fd7bf0dbe0fd09dcd119a7271f8
BradHubley <brad.hubley@dfo-mpo.gc.ca>
2017-11-15 10:01:45 -0400

    adding data

--- R/planerMap.r
+++ R/planerMap.r
@@ -1,6 +1,6 @@
 #' @export 
 
-  planerM ap = function( xyz, depths=T, pts=NULL, colpts=F, annot=NULL, annot.cex=2.2, scalebar = T, projection = "utm20", col.regions=T, datascale=seq(0,1,l=50), at=datascale,
+  planerMap = function( xyz, depths=T, pts=NULL, colpts=F, annot=NULL, annot.cex=2.2, scalebar = T, projection = "utm20", col.regions=T, datascale=seq(0,1,l=50), at=datascale,
                 fn=paste("map", trunc(runif(1)*1e8), sep=""), loc=tempdir(), corners=NULL, rez=c(1,1), save=F, pt.cex=0.5, 
                 pt.pch=16, pt.col='black',colorkey=NULL, fill=T, log.variable=F, interpolation=F, add.zeros=F, theta=50, rev=F, load.data=F, ... ) {
 
